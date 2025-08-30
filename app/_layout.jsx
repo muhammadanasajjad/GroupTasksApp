60169f8dd76c1c0c5f881eec9ce88c5752c18150
Muhammad <muhammadanasajjad@gmail.com>
2025-08-30 18:47:09 +0100

    Initial commit with expo-router setup

---
+++ app/_layout.jsx
@@ -0,0 +1,38 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+export default function index() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          height: 75,
+          borderTopWidth: 0,
+          paddingTop: 18,
+        },
+
+        // tabBarLabelStyle: { display: "none" },
+        tabBarHideOnKeyboard: true,
+        unmountOnBlur: true,
+        freezeOnBlur: false,
+        lazy: false,
+        // animation: "none",
+      }}
+      initialRouteName="home"
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="stats"
+        options={{
+          title: "Stats",
+        }}
+      />
+    </Tabs>
+  );
+}
